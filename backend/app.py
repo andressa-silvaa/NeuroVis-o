3c842c35017428f344470dd27df55df12269e910
Andressa Silva <andressacosta@pecege.com>
2025-04-02 08:10:22 -0300

    FEAT: Definindo tempo de expiração de tokens

--- backend/app.py
+++ backend/app.py
@@ -1,29 +1,24 @@
 from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
-from flask_jwt_extended import JWTManager
 from config.config import Config
-from extensions import db, jwt 
+from extensions import db, jwt  
 
 def create_app():
     app = Flask(__name__)
     app.config.from_object(Config)
     
-    # Inicializa extensões
     db.init_app(app)
     jwt.init_app(app)
     
-    # Importa e registra blueprints DENTRO da função create_app
-    from controllers.userController import user_bp
-    app.register_blueprint(user_bp)
+    with app.app_context():
+        from controllers.userController import user_bp
+        app.register_blueprint(user_bp)
+        
+        from models.userModel import User
+        db.create_all()
     
     return app
 
 app = create_app()
 
-# Import dos models deve vir DEPOIS de criar o app
-from models.userModel import User
-
 if __name__ == "__main__":
-    with app.app_context():
-        db.create_all()
     app.run(debug=True)
--- backend/config/configExample.py
+++ backend/config/configExample.py
@@ -1,5 +1,9 @@
+from datetime import timedelta
+
 class Config:
     SQLALCHEMY_DATABASE_URI = 'mssql+pyodbc://USUÁRIO:SENHA@SERVIDOR/NOME_BANCO?driver=ODBC+Driver+18+for+SQL+Server&TrustServerCertificate=yes'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SECRET_KEY = 'SUBSTITUA-POR-UMA-CHAVE-SECRETA-FORTE'
     JWT_SECRET_KEY = 'OUTRA-CHAVE-SECRETA-FORTE-AQUI'
+    JWT_ACCESS_TOKEN_EXPIRES = timedelta(minutes=30)  
+    JWT_REFRESH_TOKEN_EXPIRES = timedelta(days=7)    
