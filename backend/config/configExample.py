4959e787c0542b7217cbe9bef7162a331b7dcbaf
Andressa Silva <andressacosta@pecege.com>
2025-04-03 11:23:42 -0300

    TESTE

--- backend/config/configExample.py
+++
@@ -1,8 +0,0 @@
-from datetime import timedelta
-
-class Config:
-    SQLALCHEMY_DATABASE_URI = 'mssql+pyodbc://USUÁRIO:SENHA@SERVIDOR/NOME_BANCO?driver=ODBC+Driver+18+for+SQL+Server&TrustServerCertificate=yes'
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
-    JWT_SECRET_KEY = 'CHAVE-SECRETA-FORTE-AQUI'
-    JWT_ACCESS_TOKEN_EXPIRES = timedelta(minutes=30)  
-    JWT_REFRESH_TOKEN_EXPIRES = timedelta(days=7)    
